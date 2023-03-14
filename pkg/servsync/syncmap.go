283c4d02bda8c3919aacaf6d7d30537ad8b4c06e
Daniel Hannon <hello111f@gmail.com>
2023-03-14 16:58:49 +0000

    Added sync map wrapper to make app code less boilerplatey

---
+++ pkg/servsync/syncmap.go
@@ -0,0 +1,64 @@
+package servsync
+
+import (
+	"sync"
+	"encoding/json"
+)
+
+// This is a wrapper for a sync map to allow for JSON serialization/Deserialization
+// And to avoid typecasting in the application space
+
+type SyncMap struct {
+	smap sync.Map // The Map in question :) 
+}
+
+// Get value from map
+func (u *SyncMap) Get(key string) (val ServSync,ok bool) {
+	val = ServSync{}
+	val1, ok := u.smap.Load(key)
+	if !ok {
+		return
+	}
+	val = val1.(ServSync)
+	return
+}
+
+// Set value in map
+func (u *SyncMap) Set(key string, val ServSync) {
+	u.smap.Store(key,val)
+}
+
+func (u *SyncMap) MarshalJSON() ([]byte, error) {
+	var sMap map[string]ServSync
+
+	u.smap.Range(func(key, value any) bool {
+		if key == nil {
+			return false
+		}
+
+		mKey := key.(string)
+		mValue := value.(ServSync)
+
+		sMap[mKey] = mValue
+
+		return true
+	})
+	
+	return json.Marshal(sMap)
+}
+
+func (u *SyncMap) UnmarshalJSON(data []byte) error {
+	var sMap map[string]ServSync
+
+	if err := json.Unmarshal(data,&sMap); err != nil {
+		return err
+	}
+
+	u.smap = sync.Map{}
+
+	for key, value := range sMap {
+		u.smap.Store(key,value)
+	}
+
+	return nil
+}
